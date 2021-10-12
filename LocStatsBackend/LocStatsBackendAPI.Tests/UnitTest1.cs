899a3d248ed9694eca5c5ae4bfe75d326fa9633b
PercentEquals <bartosz.nti@gmail.com>
2021-10-12 17:31:41 +0200

    Added LocStatsBackend template #1

---
+++ LocStatsBackend/LocStatsBackendAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LocStatsBackendAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
