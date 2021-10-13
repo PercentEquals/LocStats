0cc5000966e6b0d00d0ff0292c9a0d854a09f502
SZYMON-PC\SzymonKlewicki <domeczekklewicki@gmail.com>
2021-10-13 11:16:39 +0200

    Added initial mobile app project #2

---
+++ LocStatsMobile/MobileApp/Fragments/FragmentLogIn.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MobileApp.Fragments
+{
+    public class FragmentLogIn : AndroidX.Fragment.App.Fragment
+    {
+        private Action logInCallback;
+
+        public FragmentLogIn(Action logInCallback)
+        {
+            this.logInCallback = logInCallback;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+           
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+
+            Button button = View.FindViewById<Button>(Resource.Id.buttonLogIn);
+            button.Click += logInButtonClick;
+        }
+
+        private void logInButtonClick(object sender, EventArgs e)
+        {
+            EditText editTextEmail = View.FindViewById<EditText>(Resource.Id.editTextEmail);
+            EditText editTextPassword = View.FindViewById<EditText>(Resource.Id.editTextPassword);
+
+            logInCallback();
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            
+            return inflater.Inflate(Resource.Layout.activity_log_in, container, false);
+
+        }
+    }
+}
---
+++ LocStatsMobile/MobileApp/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+using MobileApp.Fragments;
+using System.Collections.Generic;
+
+
+namespace MobileApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        List<AndroidX.Fragment.App.Fragment> fragments;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+           
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+            navigation.Visibility = ViewStates.Gone;
+
+            
+
+            fragments = new List<AndroidX.Fragment.App.Fragment>();
+
+            fragments.Add(new FragmentLogIn(logInCallback));
+            fragments.Add(new FragmentLocalization());
+            fragments.Add(new FragmentDataShow());
+
+            loadFragment(0);
+        }
+
+        private void loadFragment(int fragmentIndex)
+        {
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.frameLayoutMain, fragments[fragmentIndex])
+                .Commit();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+        private void logInCallback()
+        {
+            loadFragment(1);
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.Visibility = ViewStates.Visible;
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_localize:
+                    loadFragment(1);
+                    return true;
+
+                case Resource.Id.navigation_dashboard:
+                    loadFragment(2);
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
