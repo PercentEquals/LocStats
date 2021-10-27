665141269a8195907adebd1a44070bad9e7805a8
Bart63 <bartek.durys@gmail.com>
2021-10-27 06:54:38 +0200

    GPS data storage proto

---
+++ LocStatsMobile/MobileApp/Database/LocationRepo.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MobileApp.Database
+{
+    public class LocationRepo
+    {
+        private LocationDatabase db = null;
+        protected static LocationRepo me;
+
+        static LocationRepo()
+        {
+            me = new LocationRepo();
+        }
+
+        public LocationRepo()
+        {
+            db = new LocationDatabase();
+        }
+
+        public void AddLocation(long ts, double lat, double lon)
+        {
+            LocationModel lm = new LocationModel
+            {
+                Timestamp = ts,
+                Latitude = lat,
+                Longitude = lon
+            };
+            me.db.AddLocation(lm);
+        }
+
+        public IEnumerable<LocationModel> GetAllLocations()
+        {
+            return me.db.GetAllLocations();
+        }
+
+        public void DeleteAllLocations()
+        {
+            me.db.DeleteAllLocations();
+        }
+
+        public int GetLocationsLength()
+        {
+            return me.db.GetAllLocations().ToList().Count;
+        }
+    }
+}
