310c3b03451654ffb9efb424a58b6fe7129c486c
PercentEquals <bartosz.nti@gmail.com>
2021-11-14 21:28:23 +0100

    Added distance stats endpoint for #37

--- LocStatsBackend/LocStatsBackendAPI/Entities/Helpers/StatsHelper.cs
+++ LocStatsBackend/LocStatsBackendAPI/Entities/Helpers/StatsHelper.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Geolocation;
 using LocStatsBackendAPI.Entities.Models;
 
 namespace LocStatsBackendAPI.Entities.Helpers
 {
     public static class StatsHelper
     {
+        private static double ConvertMilesToMeters(double distance)
+        {
+            return Math.Round((double)(distance) * 1609.34, 2);
+        }
+
         public static Dictionary<DateTime, List<GpsCoordinate>> CategorizeDataByDate(List<GpsCoordinate> coordinates)
         {
             Dictionary<DateTime, List<GpsCoordinate>> dictionary = new();
@@ -37,6 +43,36 @@ namespace LocStatsBackendAPI.Entities.Helpers
             foreach (var item in dictionary)
             {
                 stats.Add(item.Key, item.Value.Last().Timestamp - item.Value.First().Timestamp);
+            }
+
+            return stats;
+        }
+
+        public static Dictionary<DateTime, double> CalcTraveledDistance(List<GpsCoordinate> coordinates)
+        {
+            var dictionary = CategorizeDataByDate(coordinates);
+            Dictionary<DateTime, double> stats = new();
+
+            foreach (var item in dictionary)
+            {
+                var dist = 0.0;
+                Coordinate? prevPoint = null;
+
+                foreach (var coord in item.Value)
+                {
+                    if (prevPoint == null)
+                    {
+                        prevPoint = new Coordinate(coord.Latitude, coord.Longitude);
+                    }
+                    else
+                    {
+                        var point = new Coordinate(coord.Latitude, coord.Longitude);
+                        dist += GeoCalculator.GetDistance((Coordinate) prevPoint, point);
+                        prevPoint = point;
+                    }
+                }
+
+                stats.Add(item.Key, dist);
             }
 
             return stats;
