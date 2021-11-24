aad15c7dc4bdabfb78771aa4bd6c1bada1096b9e
PercentEquals <bartosz.nti@gmail.com>
2021-11-24 20:17:40 +0100

    Added MostFrequentLocation endpoint with image teching for #37

--- LocStatsBackend/LocStatsBackendAPI/Controllers/StatsController.cs
+++ LocStatsBackend/LocStatsBackendAPI/Controllers/StatsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AutoMapper;
+using ClusteringKMeansLib;
 using LocStatsBackendAPI.Data;
 using LocStatsBackendAPI.Entities.Helpers;
 using LocStatsBackendAPI.Entities.Models;
@@ -60,7 +63,7 @@ namespace LocStatsBackendAPI.Controllers
                 response.Add(new StatsResponse
                 {
                     Date = from.Date.ToString("yyyy-MM-dd"),
-                    Value = time.ContainsKey(from.Date) ? time[from.Date] : 0
+                    Value = time.ContainsKey(from.Date) ? time[from.Date] : 0.0
                 });
 
                 from = from.AddDays(1);
@@ -97,13 +100,98 @@ namespace LocStatsBackendAPI.Controllers
                 response.Add(new StatsResponse
                 {
                     Date = from.Date.ToString("yyyy-MM-dd"),
-                    Value = distance.ContainsKey(from.Date) ? distance[from.Date] : 0
+                    Value = distance.ContainsKey(from.Date) ? distance[from.Date] : 0.0
                 });
 
                 from = from.AddDays(1);
             }
 
             return Ok(response);
+        }
+
+        /// <summary>
+        /// Returns most frequent location (lat and long) for specified time range
+        /// </summary>
+        /// <param name="from">Date from (eq. 2021-11-14)</param>
+        /// <param name="to">Date to (eq. 2021-11-14)</param>
+        /// <param name="clusters">How many clusters to predict</param>
+        /// <returns>Coordinates result</returns>
+        /// <response code="200">Returns most frequent location</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Something went wrong</response>
+        [HttpGet]
+        [Route("MostFrequentLocation/{from:datetime}/{to:datetime}/{clusters:int}")]
+        public async Task<IActionResult> GetMostFrequentLocation(DateTime from, DateTime to, int clusters)
+        {
+            if (!ModelState.IsValid)
+                return new BadRequestObjectResult("Invalid payload");
+            if (clusters <= 0 || clusters >= 100)
+                return new BadRequestObjectResult("Clusters size must be a non negative integer lower than 100");
+
+            var userId = User.Claims.First(i => i.Type == "Id").Value;
+            var coords = await _gpsService.GetCoordinatesFrom(from, to, userId);
+
+            if (coords.Count <= 0)
+                return new BadRequestObjectResult("Not enough coordinate data");
+            if (clusters > coords.Count)
+                return new BadRequestObjectResult("Clusters size was too big for the data from specified range");
+
+            double[][] data = new double[coords.Count][];
+            var i = -1;
+
+            foreach (var coord in coords)
+            {
+                data[++i] = new double[] { coord.Latitude, coord.Longitude };
+            }
+
+            var k = clusters;
+            var initMethod = "plusplus";
+            var maxIter = 100;
+            var seed = 0;
+            var trials = 10;
+
+            KMeans km = new KMeans(k, data, initMethod, maxIter, seed);
+            km.Cluster(trials);
+
+            var mostFrequentCluster = km.counts.Max();
+            var mostFrequentClusterIndex = km.counts.ToList().IndexOf(mostFrequentCluster);
+
+            var meanLocation = km.means[mostFrequentClusterIndex];
+
+            var url = new System.Text.StringBuilder();
+            url.Append($"https://commons.wikimedia.org/w/api.php?");
+            url.Append($"format=json&action=query&generator=geosearch&ggsprimary=all&ggsnamespace=6&ggsradius=500");
+            url.Append($"&ggscoord={ meanLocation[0] }|{ meanLocation[1] }");
+            url.Append($"&ggslimit=1&prop=imageinfo&iilimit=1&iiprop=url&iiurlwidth=200&iiurlheight=200");
+
+            var imageUrl = "";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url.ToString());
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var pattern = new Regex("\"url\":(.*?)\",");
+                    Match m = pattern.Match(content);
+
+                    if (m.Success)
+                    {
+                        imageUrl = m.Value.Substring(7);
+                        imageUrl = imageUrl.Substring(0, imageUrl.Length - 2);
+                    }
+                }
+            }
+
+            return Ok(new GpsStatsResponse
+            {
+                ImageUrl = imageUrl,
+                Latitude = meanLocation[0],
+                Longitude = meanLocation[1],
+            });
         }
     }
 }
